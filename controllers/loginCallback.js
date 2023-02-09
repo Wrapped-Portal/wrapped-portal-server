5c508de14a22d1aee42804049b81e9ecbc89de6f
Stevenrej <steveo732@gmail.com>
2023-02-09 16:06:04 -0600

    callbacks added

---
+++ controllers/loginCallback.js
@@ -0,0 +1,26 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+require('dotenv').config();
+
+exports.loginCallback = (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(error => {
+      console.log(error.message);
+      res.sendStatus(400);
+    });
+};
---
+++ controllers/refreshCallback.js
@@ -0,0 +1,27 @@
+const SpotifyWebApi = require('spotify-web-api-node');
+require('dotenv').config();
+
+exports.refreshCallback = (req, res) => {
+  const refreshToken = req.body.refreshToken;
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      console.log(data.body);
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(error => {
+      console.log(error.message);
+      res.sendStatus(400);
+    });
+};
