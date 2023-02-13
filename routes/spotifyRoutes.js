ee89997c5ab642a9857d0d583940f044e5daf2d7
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-02-13 15:32:25 -0500

    refactor to put routes in route folder
    - fix typo on recommendation route

--- routes/spotifyRoutes.js
+++ routes/spotifyRoutes.js
@@ -5,9 +5,20 @@
 
 const express = require('express');
 const getUserPlaylists = require('../controllers/spotifyAPI/playlist/getUserPlaylists');
-
+const createPlaylists = require('../controllers/spotifyAPI/playlist/createPlaylist');
+const addTrackToPlaylist = require('../controllers/spotifyAPI/playlist/addTrackToPlaylist');
+const getTopResults = require('../controllers/spotifyAPI/topResults/getTopResults');
+const getRecommendations = require('../controllers/spotifyAPI/recommendations/getRecommendations');
 const spotifyRoutes = express.Router();
 
 spotifyRoutes.route('/playlist').post(getUserPlaylists);
+
+spotifyRoutes.route('/playlist').post(createPlaylists);
+
+spotifyRoutes.route('/track').post(addTrackToPlaylist);
+
+spotifyRoutes.route('/top').get(getTopResults);
+
+spotifyRoutes.route('/recommendation').get(getRecommendations);
 
 module.exports = spotifyRoutes;
