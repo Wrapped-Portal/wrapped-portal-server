1d0d6225992c1f260ad9813bc1a021c16a156313
Stevenrej <steveo732@gmail.com>
2023-02-14 19:36:33 -0600

    added get user route

---
+++ controllers/spotifyAPI/user/getUser.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+async function getUser(req, res) {
+
+  const token = req.query.token;
+  try {
+    const options = {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+    };
+
+    const response = await axios.get('https://api.spotify.com/v1/me', options);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error retrieving user playlists' });
+  }
+}
+
+module.exports = getUser;
