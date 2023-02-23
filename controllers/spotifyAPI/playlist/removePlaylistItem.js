e16065693526462858da33253dc5963a60574574
Stevenrej <steveo732@gmail.com>
2023-02-23 15:33:12 -0600

    remove from playlist endpoint added

---
+++ controllers/spotifyAPI/playlist/removePlaylistItem.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+async function removePlaylistItem(req, res) {
+  const token = req.query.token;
+  const playlistId = req.body.playlistId;
+  const trackUri = req.body.trackUri;
+
+console.log(trackUri);
+
+  try {
+    const options = {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      data: {
+        "tracks": [
+          {
+            "uri": trackUri,
+          }
+        ]
+      }
+    };
+    
+
+    const response = await axios.delete(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, options);
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error removing tracks from playlist' });
+  }
+}
+
+module.exports = removePlaylistItem;
