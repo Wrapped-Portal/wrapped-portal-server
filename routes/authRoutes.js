5a2e00e9451cc4b033c9a61082edf8dc0c811402
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-03-03 16:49:16 -0500

    create connection route for backend

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,5 +10,7 @@ const AuthRoutes = express.Router();
 
 AuthRoutes.route('/refresh').post(refreshCallback);
 AuthRoutes.route('/login').post(loginCallback);
-
+AuthRoutes.route('/connect').get((req, res, next) =>
+  res.status(200).send({ connected: 'connected' }),
+);
 module.exports = AuthRoutes;
